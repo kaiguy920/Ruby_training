8d78286bcd938bc668d36be8c439ce34fa743c46
kaiguy920 <smithkai920@gmail.com>
2022-12-02 15:40:53 -0500

    adds start to quiz

---
+++ SeleniumRuby/quiz1.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# 1. Create a simple program in Ruby that generates a random number (1-100) by looking into ruby-doc.org and using that random number, find if you are lucky winner. If you get a lucky number, then display you are a winner if not please try again!. Hint - Lucky winner can be any number you would want.
+
+
+
+
+# 2. Create a program in Ruby using switch(case) and display if the day is holiday or weekday.
+#
+# Hint: Holiday = Saturday and Sunday
