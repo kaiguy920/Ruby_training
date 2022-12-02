8ea24904553e156e147c6610c41478e884aadfde
kaiguy920 <smithkai920@gmail.com>
2022-12-02 09:42:56 -0500

    adds additional string methods

--- SeleniumRuby/strings.rb
+++ SeleniumRuby/strings.rb
@@ -30,7 +30,6 @@ puts "==========! changes variable ==============="
 puts state.downcase!
 puts state
 
-=end
 
 #DELETE - deletes the character string value from previous string
 country = 'Denmark'
@@ -40,3 +39,40 @@ puts country.delete('dk') # => "Denmar" ==> only deletes k
 
 country2 = "United States"
 puts country2.delete("e") # => "Unitd Stats" ==> deletes all "e"
+
+#REPLACE -  replaces the original string with new string value
+county = "Fairfax"
+puts county.replace("Polk")
+
+#CAPITALIZE - affects first letter of string
+color = "yellow"
+puts color.capitalize
+
+color2 = "PURPLE"
+puts color2.capitalize
+
+#REVERSE - reverses the characters of the string
+fruit = 'desserts are not fruit'
+puts fruit.reverse
+
+#LENGTH - counts the # of characters in a string
+song = "This is my life"
+puts song.length # = 15 ==> counts " "
+
+#STRIP - removes white spaces on the L & R side of the string
+song2 = "   Welcome to my life      "
+#LSTRIP - removes white spaces on L side
+#RSTRIP - removes white spaces on R side
+puts song2
+puts song2.strip
+print "lstrip==> ",  song2.lstrip
+print "rstrip==> ", song2.rstrip
+=end
+
+#GSUB - substitutes the string value with the new string
+noun = "people, places, things"
+puts noun
+puts noun.gsub("things", "thangs")
+puts "==========! changes variable ==============="
+puts noun.gsub!("things", "thangs")
+puts noun
