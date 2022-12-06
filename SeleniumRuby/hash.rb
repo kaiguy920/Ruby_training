4fe5dd3033dbc0c8dc0291853ab1aede81478ab7
kaiguy920 <smithkai920@gmail.com>
2022-12-06 10:21:07 -0500

    finishes has lesson

--- SeleniumRuby/hash.rb
+++ SeleniumRuby/hash.rb
@@ -19,3 +19,38 @@ ssn = {"George Michael" => 2468,
        "Randy David" => 6486}
 puts ssn.key(3698)
 puts ssn.key(6486)
+
+#=============================================================
+h = Hash.new
+h["brandy"] = 3.9
+h["ashton"] = 2.9
+h["steve"] = 4.0
+
+puts h
+
+#=============================================================
+# loop over hash
+for key in ssn.keys
+  # puts "#{key}"
+  if key == "Mary Smith"
+    puts "Welcome to Ruby Selenium class #{key}"
+  end
+end
+
+ssn.each do |key, value|
+  puts "#{key}'s ssn is: #{value}'"
+end
+
+#=============================================================
+celebrity_hash = {'Beyonce' => 252424,
+                  'Michael Jackson' => 4243433,
+                  'Sam Smith' => 52134434,
+                  'Taylor Swift' => 7534343,
+                  'Tommy Smith' => 523232}
+for key in celebrity_hash.keys
+  # puts "Celeb - #{key}"
+end
+
+celebrity_hash.each do |key, value|
+  puts "#{value}"
+end
